b3edb57b1b443f013eff68746749d51dbd39ab4a
Giancarlo Josep Choque Ñiquin <134126104+Giancarlo44@users.noreply.github.com>
2025-06-11 10:20:26 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Gobierno anuncia subsidio para enfrentar alza de precios de alimentos",
+"autor" => "Andina",
+"resumen" => "El Ejecutivo peruano otorgará un subsidio económico a familias vulnerables para mitigar el impacto del alza en los precios de productos básicos.",
 ];
 ?>
