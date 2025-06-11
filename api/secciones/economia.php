8c65889049991e856c4e767b6e1e8a43d2cb3809
Giancarlo Josep Choque Ñiquin <134126104+Giancarlo44@users.noreply.github.com>
2025-06-11 10:22:50 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "FMI eleva proyección de crecimiento para América Latina en 2025",
+"autor" => "Bloomberg",
+"resumen" => "El FMI ajustó al alza sus previsiones de crecimiento económico para América Latina, impulsado por la recuperación de Brasil y México.",
 ];
 ?>
