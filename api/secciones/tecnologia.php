7befe3a88b0e130364dc847855aea68e7a5a64a5
Giancarlo Josep Choque Ñiquin <134126104+Giancarlo44@users.noreply.github.com>
2025-06-11 10:23:36 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Apple presenta iOS 26 con mejoras en inteligencia artificial",
+"autor" => "The Verge",
+"resumen" => "Apple lanzó iOS 19 con nuevas funciones impulsadas por IA, enfocadas en personalización, seguridad y productividad.",
 ];
 ?>
