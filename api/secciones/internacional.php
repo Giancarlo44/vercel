236f668a75c181befcf74c8b95cbd3bda9843090
Giancarlo Josep Choque Ñiquin <134126104+Giancarlo44@users.noreply.github.com>
2025-06-11 10:18:42 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Acuerdo histórico entre Ucrania y la OTAN refuerza apoyo militar",
+"autor" => "Agencia Reuters",
+"resumen" => "Ucrania firmó un acuerdo con la OTAN para fortalecer la cooperación militar y recibir más apoyo defensivo frente a Rusia.",
 ];
 ?>
